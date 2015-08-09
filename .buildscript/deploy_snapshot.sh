b49949a1123027b4f34ccf53e8a71080a26188a2
Jens Driller <jens@jensdriller.com>
2015-08-09 16:37:32 +0100

    Only deploy SNAPSHOTS from develop with JDK8

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-mvn clean deploy --settings=".buildscript/settings.xml"
+JDK_VERSION="oraclejdk8"
+BRANCH="develop"
+
+if [ "$TRAVIS_JDK_VERSION" != "$JDK_VERSION" ]; then
+    echo "Skipping snapshot deployment: Only deploying with '$JDK_VERSION'."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: Only deploying from '$BRANCH'."
+elif [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  echo "Skipping snapshot deployment: Not deploying pull requests."
+else
+    mvn clean deploy --settings=".buildscript/settings.xml"
+fi
