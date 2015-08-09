5cc4ef7e28289464b3558bd7a6f075c0f29d3cb4
Jens Driller <jens@jensdriller.com>
2015-08-09 22:24:17 +0100

    Add unit tests for default values

--- sample/src/main/java/com/jenzz/noop/sample/TestInterface.java
+++ sample/src/main/java/com/jenzz/noop/sample/TestInterface.java
@@ -6,28 +6,22 @@ import com.jenzz.noop.annotation.NoOp;
 public interface TestInterface {
 
     byte aByte();
-    Byte aByteObject();
 
     short aShort();
-    Short aShortObject();
 
     int anInt();
-    Integer anIntObject();
 
     long aLong();
-    Long aLongObject();
 
     float aFloat();
-    Float aFloatObject();
 
     double aDouble();
-    Double aDoubleObject();
 
     char aChar();
-    Character aCharObject();
 
     boolean aBoolean();
-    Boolean aBooleanObject();
+
+    String aString();
 
     void aVoid();
 }
